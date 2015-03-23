a979dc749eafac50f3205aa7b92cdf6610285954
Sheharyar Naseer <sheharyar92@live.com>
2015-03-23 16:32:03 +0500

    Updated `zippy` to v0.0.3

--- Formula/zippy.rb
+++ Formula/zippy.rb
@@ -1,7 +1,7 @@
 class Zippy < Formula
   homepage "https://github.com/sheharyarn/zippy"
-  url "https://github.com/sheharyarn/zippy/archive/v0.0.2.tar.gz"
-  sha256 "c6ebd63f6dfbfe1b79b0541ec936ed9ff190189fc112285603325a7e0766b324"
+  url "https://github.com/sheharyarn/zippy/archive/v0.0.3.tar.gz"
+  sha256 "5c1c8d9399843b9646798b36029464b915ca636fd7e9a766f5cdc3f4d8d25704"
 
   depends_on 'wget'
 
