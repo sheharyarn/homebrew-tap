85a697fde075f22ec5cf0153a2b8212bd3981150
Sheharyar Naseer <hello@sheharyar.me>
2015-10-04 20:49:45 +0500

    Updated vncviewer to v0.2

--- Formula/vncviewer.rb
+++ Formula/vncviewer.rb
@@ -1,7 +1,7 @@
 class Vncviewer < Formula
   homepage "https://github.com/sheharyarn/vncviewer"
-  url "https://github.com/sheharyarn/vncviewer/archive/v0.1.tar.gz"
-  sha256 "c3d28e0a5dc489e8756760fee68d416c7f532dfe52595a6b425ca40532de95d8"
+  url "https://github.com/sheharyarn/vncviewer/archive/v0.2.tar.gz"
+  sha256 "956c35fa9a218b1227227aa8ab233af659cf545a0b5279a82914c6f4ed982bd2"
 
   def install
     bin.install 'vncviewer'
